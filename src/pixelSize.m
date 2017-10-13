a438a461b6b3c78fb5282e15bf098c00722583cd
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-13 23:38:07 +0200

    Adds first sources

---
+++ src/pixelSize.m
@@ -0,0 +1,26 @@
+function [pSize] = pixelSize(imagePath, i)
+    image = imread(imagePath);
+    
+    hSize = size(image, 2);
+    vSize = size(image, 1);
+    
+    
+    pSize = hSize * vSize + 1;
+    
+    for i = 1 : vSize
+        
+        count = 1;
+        
+        for j = 2 : hSize
+            diff = int16(image(i, j, :)) - int16(image(i, j-1, :));
+            diff = reshape(diff, 1, 3);
+            if norm(double(diff)) >= i
+                pSize = min(pSize, count);
+                count = 1;
+            else
+                count = count + 1;
+            end
+            
+        end
+    end
+end
---
+++ src/test1.m
@@ -0,0 +1,7 @@
+clc;
+
+pSize = zeros(1, 442);
+
+for i = 1 : 442
+    pSize(i) = pixelSize('super_mario_bros_01.jpg');
+end
