fa3fcb15c92af2589d30a75153cf75efdf15441c
Daryan7 <juanmiguel1996@gmail.com>
2017-10-14 19:08:51 +0200

    More scripts, things seem to work

--- src/functions/generateObjects.m
+++ src/functions/generateObjects.m
@@ -1,7 +1,7 @@
-function [] = generateObjects(I)
+function [] = generateObjects(I,start)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 Ibin = abs(findEdges(I))>0;
-extractObjects(Ibin(3:end-2,3:end-2), I(2:end-1,2:end-1,:));
+Ibin = fillGaps(Ibin(3:end-2,3:end-2),start);
+extractObjects(Ibin, I(2:end-1,2:end-1,:));
 end
-
