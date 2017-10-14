7febcd44cbba2a7c1ab7f3c9c9ff6f3fc9848d77
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-14 14:33:03 +0200

    Updates project structure

--- src/scripts/colorLayerSeparationScript.m
+++ src/scripts/colorLayerSeparationScript.m
@@ -1,5 +1,6 @@
 clc;
-addpath('../samples/super_mario_bros/');
+addpath('../functions/');
+addpath('../../samples/super_mario_bros/');
 imageBasePath = 'super_mario_bros_';
 
 for i = 1 : 1
@@ -17,15 +18,15 @@ for i = 1 : 1
         
         if i < 10
             if j < 10
-                imwrite(image, strcat('../output/', 'layer_', imageBasePath, '0', num2str(i), '_0', num2str(j), '.png'));
+                imwrite(image, strcat('../../output/', 'layer_', imageBasePath, '0', num2str(i), '_0', num2str(j), '.png'));
             else
-                imwrite(image, strcat('../output/', 'layer_', imageBasePath, '0', num2str(i), '_', num2str(j), '.png'));
+                imwrite(image, strcat('../../output/', 'layer_', imageBasePath, '0', num2str(i), '_', num2str(j), '.png'));
             end
         else
             if j < 10
-                imwrite(image, strcat('../output/', 'layer_', imageBasePath, num2str(i), '_0', num2str(j), '.png'));
+                imwrite(image, strcat('../../output/', 'layer_', imageBasePath, num2str(i), '_0', num2str(j), '.png'));
             else
-                imwrite(image, strcat('../output/', 'layer_', imageBasePath, num2str(i), '_', num2str(j), '.png'));
+                imwrite(image, strcat('../../output/', 'layer_', imageBasePath, num2str(i), '_', num2str(j), '.png'));
             end
         end
     end
---
+++ src/scripts/flattenColorScript.m
@@ -0,0 +1,12 @@
+clc;
+clc;
+addpath('../functions/');
+addpath('../../samples/');
+addpath('../../samples/super_mario_bros/');
+imageBasePath = 'super_mario_bros_';
+imagePath = strcat(imageBasePath, '12.png');
+imageSavePath = strcat('../../output/', 'flatten_', imagePath);
+
+image = flattenColor(imagePath);
+
+imwrite(image, imageSavePath);
