fc457422ad3610e755796fffa2cc74d8085173f3
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-15 03:44:06 +0200

    Updates servers output directory

--- src/propCV/functions/serverExtract.m
+++ src/propCV/functions/serverExtract.m
@@ -1,14 +1,19 @@
 function [Iout] = serverExtract(s)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-fid = fopen('../../common/images/decodedImage.jpg', 'wb');
+
+if ~exist('../../../output/servers/', 'dir')
+    mkdir '../../../output/servers/';
+end
+
+fid = fopen('../../../output/servers/decodedImage.jpg', 'wb');
 fwrite(fid, s, 'uint8');            % dump the raw binary to the hard disk
 fclose(fid);
 I = imread('decodedImage.jpg');
 numObjects = generateObjects(I);
 Iout = uint32(zeros(numObjects,1));
 for i = 1:numObjects
-    fid = fopen(strcat('../../common/images/object_',num2str(i),'.png'));
+    fid = fopen(strcat('../../../output/servers/object_',num2str(i),'.png'));
     A = uint32(fread(fid));
     Iout(i,1) = size(A,1);
     Iout(i,2:size(A,1)+1) = A;
--- src/propCV/functions/serverLayer.m
+++ src/propCV/functions/serverLayer.m
@@ -1,15 +1,20 @@
 function [Iout] = serverLayer(s)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-fid = fopen('../../common/images/decodedImage.jpg', 'wb');
+
+if ~exist('../../../output/servers/', 'dir')
+    mkdir '../../../output/servers/';
+end
+
+fid = fopen('../../../output/servers/decodedImage.jpg', 'wb');
 fwrite(fid, s, 'uint8');            % dump the raw binary to the hard disk
 fclose(fid);
 I = imread('decodedImage.jpg');
 imLayers = colorLayerSeparation(I);
 Iout = uint32(zeros(size(imLayers,1),1));
 for i = 1:size(imLayers,1)
-    imwrite(imLayers(i,:,:,:),'../../common/images/temp.png');
-    fid = fopen('../../common/images/temp.png');
+    imwrite(imLayers(i,:,:,:),'../../../output/servers/temp.png');
+    fid = fopen('../../../output/servers/temp.png');
     A = uint32(fread(fid));
     Iout(i,1) = size(A,1);
     Iout(i,2:size(A,1)+1) = A;
