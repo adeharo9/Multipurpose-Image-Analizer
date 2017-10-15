395b30b6c22b736e85cce037d2cc7a1301b38b2a
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-15 02:47:47 +0200

    Adds server interfaces

---
+++ src/propCV/functions/colorLayerSeparation.m
@@ -0,0 +1,11 @@
+function [imageLayers] = colorLayerSeparation(image)
+    vSize = size(image, 1);
+    hSize = size(image, 2);
+    
+    colorFrequence = getColorsByFrequence(image);
+    imageLayers = zeros(size(colorFrequence, 1), vSize, hSize, 3);
+    
+    for i = 1 : size(colorFrequence, 1)
+        imageLayers(i, :, :, :) = selectColor(image, colorFrequence(i, :));
+    end
+end
