b6571bdb9b1a26fcd95cbe030e8035008678c57a
Alejandro de Haro <a.deharo9@gmail.com>
2017-10-15 02:08:45 +0200

    Updates sample names

--- src/propCV/scripts/flattenColorScript.m
+++ src/propCV/scripts/flattenColorScript.m
@@ -1,10 +1,11 @@
-clc;
+clear;
+close all;
 clc;
 addpath('../functions/');
 addpath('../../common/functions/');
 addpath('../../../samples/propCV/');
 imageBasePath = 'propCV_';
-imagePath = strcat(imageBasePath, '12.png');
+imagePath = strcat(imageBasePath, '11.png');
 imageSavePath = strcat('../../../output/propCV/', 'flatten_', imagePath);
 
 image = flattenColor(imagePath);
